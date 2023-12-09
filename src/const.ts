080508b1d4c3274d92c5903b177f64842325c154
Pluto Rotegott <rotegott@dogma-project.org>
2023-12-09 09:35:52 +0200

    fix api path

--- src/const.ts
+++ src/const.ts
@@ -1,2 +1,4 @@
-export const API_PATH = "http://localhost:4444/api"; // edit
-export const SSE_PATH = "http://localhost:4444/events"; // edit
+export const API_BASE = import.meta.env.DEV ? "http://localhost:4444" : ""; // edit
+
+export const API_PATH = API_BASE + "/api";
+export const SSE_PATH = API_BASE + "/events";
